c6d195ebb234fa9b3be4d53d85672b0bc60e9e52
juliana-lab <61954927+juliana-lab@users.noreply.github.com>
2023-11-22 13:04:07 -0600

    helloooooo

---
+++ config.py
@@ -0,0 +1,13 @@
+# API Keys
+OPENAI_API_KEY   = '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'
+CLIPDROP_API_KEY = '77e7959515bc13ce5dace14002316c99625f776426063e76ed87c6014e6c0825724852f652706076368961b013e15a80'
+
+# Messages for the ChatGPT RPG instance
+CHATGPT_RPG_MESSAGES =[
+    {
+      "role": "system",
+
+      "content": "Act like an engineering tutor for a student with ADHD. Break your answers into bullet points and make it engaging, so the student don't lose interest over the topic provided, when possible provide examples of applications of the topic in engineering."
+    },
+
+  ]
